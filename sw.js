253d7b949e047bea542c2d12055c2c781e7c4a78
Leonardo Pereira de Souza <leo.dpsouza@gmail.com>
2026-09-14 17:41:28 -0300

    Feature

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "treino-cache-v2";
+const CACHE_NAME = "treino-cache-v4";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
